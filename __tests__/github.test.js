f3c16635acef2d168777002c2a1a1a9e0dee01f3
Erik M Baxstrom <baxstrem@gmail.com>
2022-12-06 12:49:31 -0800

    add test for github login route and test passing

---
+++ __tests__/github.test.js
@@ -0,0 +1,23 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+jest.mock('../lib/services/github-service');
+
+describe('github auth', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('/api/v1/github/login should redirect to github oauth', async () => {
+    const res = await request(app).get('/api/v1/github/login');
+    expect(res.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d.]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
+    );
+  });
+});
