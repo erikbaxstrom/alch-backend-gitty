352e56f3c9cb5b37f37bd3fba0b028d2dc6bd73c
Erik M Baxstrom <baxstrem@gmail.com>
2022-12-05 17:58:24 -0800

    implement insertion of user if they dont already exist

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,11 +1,9 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS github_users CASCADE;
 
-CREATE TABLE users (
+CREATE TABLE github_users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  email VARCHAR,
-  password_hash VARCHAR NOT NULL,
-  first_name VARCHAR NOT NULL,
-  last_name VARCHAR NOT NULL
+  login VARCHAR NOT NULL,
+  avatar VARCHAR NOT NULL
 );
