b8e26d6ed28635b45fce210ff873e94429615cc5
Erik M Baxstrom <baxstrem@gmail.com>
2022-12-07 15:19:29 -0800

    add tests and sql setup for posts routes

--- __tests__/posts.test.js
+++ __tests__/posts.test.js
@@ -2,7 +2,6 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const { getByLogin } = require('../lib/models/GithubUser.js');
 
 jest.mock('../lib/services/github-service');
 
@@ -15,18 +14,23 @@ describe('github auth', () => {
     pool.end();
   });
 
-  it('GET /api/v1/posts for logged out should return a 401 error', async () => {
+  it('GET /api/v1/posts for logged out user should return a 401 error', async () => {
     const response = await request(app).get('/api/v1/posts');
     expect(response.status).toBe(401);
   });
 
-  it('GET /api/v1/posts for logged out should return a list of posts', async () => {
+  it('GET /api/v1/posts for logged in user should return a list of posts', async () => {
     // log in
-    const [agent] = await getByLogin();
+    const agent = request.agent(app);
+    await agent.get('/api/v1/github/callback?code=42').redirects(1);
+    console.log('agent::', agent);
     // get the route
     const response = await agent.get('/api/v1/posts');
+    console.log('test posts response::', response);
+
     //expect list back
     expect(response.status).toBe(200);
+    expect(response.body).toMatchInlineSnapshot();
   });
 
   it.skip('POST for logged out should 401 error', async () => {
--- sql/setup.sql
+++ sql/setup.sql
@@ -24,5 +24,18 @@ CREATE TABLE posts (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   detail VARCHAR(255) NOT NULL,
   user_id BIGINT,
-  FOREIGN KEY (user_id) REFERENCES github_users(id)
-)
+  FOREIGN KEY (user_id) REFERENCES github_users(id));
+
+
+INSERT INTO
+  github_users (login, avatar)
+VALUES
+  ('Velcro', 'https://placebear.com/300/300'),
+  ('Billy Shears', 'https://placebear.com/300/300');
+
+INSERT INTO
+  posts (detail, user_id)
+VALUES
+  ('I can haz code?', 1),
+  ('What would you think if I coded a tune?', 2),
+  ('I ate a slug', 1);
